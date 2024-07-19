2888570db15cc123baedcdbc9909ef1c54f52900
Franxx20 <fgarcete@alumno.unlam.edu.ar>
2024-07-19 15:22:09 -0300

    added library

--- node/express-prisma-typescript/src/server.ts
+++ node/express-prisma-typescript/src/server.ts
@@ -26,6 +26,22 @@ app.use(
   })
 )
 
+/* socket.io
+1) el servidor inicia el servidor de socket.io
+2) el usuario inicia sesion (desde su respectivo endpoint)
+3) el usuario usuario inicia sesion mandando como parametros su su id, token (desde postman)
+4) el servidor verifica que dicho usuario verifica haya iniciado sesion haciendo
+uso de un middleware y devuelve el socket de conexion
+5) el usuario envia un mensaje que ademas posee como parametros su id y el id del usuario con el que se
+quiere comunicar (desde postman)
+6) el servidor verifica que ya hay creado un room para el id usuario y id usuario 2
+si no existe uno el servidor crea un canal para ambos usuarios, guarda el mensaje en la base de datos
+y emite el mensajen dentro del room
+7) se repite el paso 5) y 6) estableciendo una comunicación entre ambos usuarios
+8) el usuario o el servidor finalizar la conexión
+9) fin de caso de uso.
+*/
+
 app.use('/api', router)
 
 app.use(ErrorHandling)
