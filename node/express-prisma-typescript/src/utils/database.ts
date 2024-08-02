450db74f201114db11def4f8da409f65f5f21a76
Franxx20 <fgarcete@alumno.unlam.edu.ar>
2024-08-02 11:05:17 -0300

    prisma url update

--- node/express-prisma-typescript/src/utils/database.ts
+++ node/express-prisma-typescript/src/utils/database.ts
@@ -1,5 +1,3 @@
 import { PrismaClient } from '@prisma/client';
-// import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-// import { Constants, myS3 } from '@utils';
 
 export const db = new PrismaClient();
